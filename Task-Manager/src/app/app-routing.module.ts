f37940911f0e32e743414ffdddfadd3205c45cb9
krahul2024 <sandipkumar2024@gmail.com>
2024-01-21 01:14:50 +0530

    added a new component

--- Task-Manager/src/app/app-routing.module.ts
+++ Task-Manager/src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component'; 
 import { RegisterComponent } from './components/register/register.component';  
-import { HomeComponent } from './components/home/home.component';  
+import { HomeComponent } from './components/home/home.component'; 
+import { ChatComponent } from './components/chat/chat.component';  
 
 const routes: Routes = [
     { path : 'login', component : LoginComponent }, 
     { path : 'register', component : RegisterComponent }, 
+    { path : 'chat', component : ChatComponent }, 
     { path : '**', component : HomeComponent , pathMatch : 'full'}, 
+    
 ];
 
 @NgModule({
--- Task-Manager/src/app/app.module.ts
+++ Task-Manager/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms'; 
+import {RouterModule } from '@angular/router'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { ChatComponent } from './componets/chat/chat.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +16,13 @@ import { HomeComponent } from './components/home/home.component';
     LoginComponent,
     RegisterComponent,
     HomeComponent,
+    ChatComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
-    FormsModule
+    FormsModule, 
+    RouterModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
